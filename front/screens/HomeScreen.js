978295657fd0f5427c36af4c970b758fcde4b1f4
flora <flora.marteau@live.fr>
2018-03-12 18:09:39 +0100

    recup erreur de branche

--- front/screens/HomeScreen.js
+++ front/screens/HomeScreen.js
@@ -6,29 +6,6 @@ export default class HomeScreen extends React.Component {
     title: 'Shows',
   };
 
-  constructor() {
-    super();
-    this.state = {
-      shows: []
-    }
-  }
-
-  componentDidMount() {
-    fetch('https://api.themoviedb.org/3/discover/tv?api_key=57d6fa067a2b6c52e97ec557f764514f&language=en-US&sort_by=vote_average.desc&without_genres=false&include_null_first_air_dates=false')
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      console.log(data);
-      this.setState(shows: data);
-      console.log(this.state.shows);
-    })
-    .catch(function(error) {
-      console.log('Request failed', error)
-    });
-
-  }
-
   render() {
     return (
       <ScrollView style={styles.container}>
