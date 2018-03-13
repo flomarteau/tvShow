e1677b5ff12fc5172397fd34fd101473a46f05d9
flora <flora.marteau@live.fr>
2018-03-13 09:13:41 +0100

    boucle affichage series sans images pour l'instant

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var request = require('request');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/movies', function(req, res, next) {
+  request(
+    "https://api.themoviedb.org/3/discover/tv?api_key=57d6fa067a2b6c52e97ec557f764514f&language=en-US&sort_by=vote_average.desc&without_genres=false&include_null_first_air_dates=false",
+    function(error, response, body) {
+      body = JSON.parse(body);
+      res.json(body.results);
+    });
 });
 
 module.exports = router;
--- front/screens/HomeScreen.js
+++ front/screens/HomeScreen.js
@@ -1,18 +1,66 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Text } from 'react-native';
+import { Card, Button } from 'react-native-elements'
+
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Shows',
   };
 
+  constructor() {
+    super();
+    this.state = {
+      shows: []
+    }
+  }
+
+  componentDidMount() {
+    // var URL = 'http://127.0.0.1:3000/';
+    var ctx = this;
+    // fetch data from back route
+    fetch('http://127.0.0.1:3000/movies')
+      .then(function(response) {
+        // console.log(response);
+        return response.json();
+      })
+      // update current state with shows from the api
+      .then(function(data) {
+        ctx.setState({shows: data});
+        // console.log(data);
+      })
+      // catcher error if there is any
+      .catch(function(error) {
+        console.log('Request failed', error);
+      })
+  }
+
   render() {
+
+    var shows =[];
+    // var imgMovie = "http://image.tmdb.org/t/p/original/";
+    for (var i=0; i<this.state.shows.length; i++) {
+
+    // shows.forEach(function(shows, i){
+       shows.push(
+         <Card
+           key={i}
+           title={this.state.shows[i].name}
+           // image={require("http://image.tmdb.org/t/p/original/h1AaHftlM5Qp4qqHWJzFyDLtqxk.jpg")}
+           // image={require('../images/robot-pro.png')}
+          >
+           <Text style={{marginBottom: 10}}>
+             {this.state.shows[i].overview}
+           </Text>
+         </Card>
+       );
+     }
+
+
     return (
       <ScrollView style={styles.container}>
 
-        <Text>
-          Blabla homepage
-        </Text>
+        { shows }
 
       </ScrollView>
     );
